a955fffa769abf79fcf493cb62b346a7ab726cb4
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-07-31 15:49:23 +0200

    Aggiunto file main.js e cambiato titoli

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*Playlist*/
+document.getElementById('musica-camera').innerHTML = 'Musica da camera';
+document.getElementById('blues').innerHTML = 'Blues';
+document.getElementById('rock').innerHTML = 'Rock';
+document.getElementById('metal').innerHTML = 'Metal';
+document.getElementById('classica').innerHTML = 'Musica Classica';
+document.getElementById('swing').innerHTML = 'Swing Elettronica';
+document.getElementById('techno').innerHTML = 'Techno';
+
+/*username*/
+document.getElementById('username').innerHTML = 'Roberto Del Toro';
+
+/*circle-box*/
+document.getElementById('lana').innerHTML = 'Lana Del Rey';
+document.getElementById('ennio').innerHTML = 'Ennio Morricone';
